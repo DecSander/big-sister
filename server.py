6bfb050af1b75dadad49b7a1f278eb598ff77837
Declan Sander <dms497@cornell.edu>
2018-02-12 15:00:52 -0500

    s3

--- server.py
+++ server.py
@@ -1,9 +1,37 @@
 from flask import Flask, request
 import json
+import boto3
+import time
 app = Flask(__name__)
 
 
 most_recent_value = 0
+
+s3 = boto3.client('s3')
+
+
+def upload_file_to_s3(file):
+    timer = time.time()
+    try:
+        s3.upload_fileobj(
+            file,
+            'cc-proj',
+            str(int(timer)) + '.jpeg',
+            ExtraArgs={"ContentType": file.content_type}
+        )
+        return "{}".format(timer)
+    except Exception as e:
+        print("Something Happened: ", e)
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    try:
+        imagefile = request.files.get('imagefile', '')
+        upload_file_to_s3(imagefile)
+        return 'true'
+    except Exception as err:
+        print(err)
 
 
 @app.route("/", methods=['POST'])
@@ -20,4 +48,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
