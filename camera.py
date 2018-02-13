924366c52e3664ca90bf24b840d763e34ffbf6fc
Declan Sander <dms497@cornell.edu>
2018-02-12 21:24:40 -0500

    camera

--- camera.py
+++ camera.py
@@ -1,17 +1,26 @@
 import requests
 import os
+import json
 
 
 SERVER_URL = 'http://18.221.18.72:5000'
+server_urls = []
 
 
 def send_image(pic, f):
     files = {'imagefile': (os.path.basename(pic), f, 'image/jpeg')}
-    result = requests.post(SERVER_URL, files=files)
-    print result.status_code
+    requests.post(SERVER_URL, files=files)
+
+
+def get_urls():
+    result = requests.get(SERVER_URL + '/servers', timeout=3)
+    global server_urls
+    server_urls = json.loads(result.text)
 
 
 if __name__ == "__main__":
-    pic = '/Users/Sander/Desktop/Other/Pictures_Movies/5k/IMG_4794.JPG'
-    with open(pic, 'r') as file:
-        send_image(pic, file)
+    # pic = '/Users/Sander/Desktop/Other/Pictures_Movies/5k/IMG_4794.JPG'
+    # with open(pic, 'r') as file:
+    #     send_image(pic, file)
+    get_urls()
+    print(server_urls)
