eab983da501ac9bc0594f695019c8105f5c5238c
ericmdai <emd88@cornell.edu>
2018-03-09 16:07:43 -0500

    Adding threading

--- camera.py
+++ camera.py
@@ -8,6 +8,7 @@ import os
 from utility import bootup_camera
 from const import servers
 from picamera import PiCamera
+from threading import thread
 
 
 SERVER_URL = 'http://18.221.18.72:5000'
@@ -15,34 +16,47 @@ server_urls = servers
 
 camera_id = 1
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-
-time.sleep(2)  # Camera warm-up time
-
-
-def take_picture(file="foo.jpg"):
-    camera.capture(file)
-
-
-def send_image(f):
-    files = {'imagefile': (f, open(os.path.basename(f), 'rb'), 'image/jpeg')}
-    requests.post(SERVER_URL, files=files, data={"camera_id": camera_id, "photo_time": time.time()}, timeout=1)
-
 
 def get_urls():
     bootup_camera(server_urls)
+
+
+def start_camera_thread()
+    c = Camera()
+    c.start()
+
+
+class Camera(Thread):
+
+    def __init__(self, camera):
+        Thread.__init__(self)
+        self.camera = camera
+
+    def take_picture(self):
+        self.photo_time = time.time()
+        self.camera.capture(self.image_name)
+        self.image_name = str(self.photo_time) + ".jpg"
+
+    def send_image(self):
+        files = {'imagefile': (self.image_name, open(os.path.basename(f), 'rb'), 'image/jpeg')}
+        requests.post(SERVER_URL, files=files, data={"camera_id": camera_id, "photo_time": time.time()})
+
+    def run(self):
+        print "Taking picture..."
+        self.take_picture()
+        print "Sending picture..."
+        self.send_image()
 
 
 if __name__ == "__main__":
     get_urls()
     print(server_urls)
 
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    time.sleep(2)  # Camera warm-up time
+
     while True:
-        print "Taking picture..."
-        take_picture("picture.jpg")
-        print "Sending picture..."
-        send_image("picture.jpg")
-        print "Picture sent, waiting..."
+        start_camer_thread(camera)
         time.sleep(1)
