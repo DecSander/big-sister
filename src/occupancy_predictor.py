f3d2bd9eb7e8e6886ae78e2039cbbc00b7513e22
James Stoyell <jms852@cornell.edu>
2018-04-12 14:43:49 -0400

    additional occupancy predictor code

--- src/occupancy_predictor.py
+++ src/occupancy_predictor.py
@@ -17,23 +17,33 @@ HISTORY_UPDATE_TIME = 60*60*24 # Daily
 app = Flask(__name__)
 
 history = None
+NUM_QUERY_WEEKS = 4 # About the last month is probably a good predictor
+
+def mean(l):
+    return sum(l)/len(l)
 
 @app.route('/')
-@require_form({'day': int, 'hour': int})
+@require_form({'day': int, 'hour': int, 'camera_id': int})
 @handle_errors
-def predict_occupancy(day, hour):
+def predict_occupancy(day, hour, camera_id):
     if not history:
         return jsonify({"error" : "History is not initialized, cannot infer"}), 400
 
     prediction = 0
-    x = 0.5
-    s = 0
-    for week in history:
-        prediction += week[day][hour] * x
-        s += x
-        x /= 2
+    total_weight = 0
+    # Using a weighted prediction algorithm where the first week is worth 4, then 2, then 4/3, then 1, etc.
+    # More recent weeks are more relevant, but we don't want the most recent week being an outlier to skew the
+    # result to enormously.
+    for i, week in enumerate(history[camera_id][:NUM_QUERY_WEEKS]):
+        if day in week and hour in week[day]:
+            prediction += mean(week[day][hour]) * (NUM_QUERY_WEEKS/(i+1))
+            total_weight += (NUM_QUERY_WEEKS/(i+1))
 
-    return jsonify(prediction/s)
+    if total_weight == 0:
+        # This means we had no data to predict from
+        return jsonify(None), 204
+
+    return jsonify(prediction/total_weight)
 
 
 def get_history():
